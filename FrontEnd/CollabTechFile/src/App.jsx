debd45472b0aa5871fa7bc2c5280eb8a97e3b6d9
Kaue Moura <Kauehago07@gmail.com>
2025-10-02 16:47:13 -0300

    feat: Tela de inicio, modal, menu lateral concluidos

--- FrontEnd/CollabTechFile/src/App.jsx
+++ FrontEnd/CollabTechFile/src/App.jsx
@@ -2,15 +2,20 @@ import { useState } from 'react'
 import './App.css'
 // import Login from './pages/login/Login'
 import Inicio from './pages/Inicio/Inicio'
+import TelaCliente from './pages/telaCliente/telaCliente'
+import ModalFiltroFuncionario from './pages/filtroFuncionario/modalFiltroFuncionario'
 // import ListagemDoc from './pages/listagemDoc/listagemDoc'
+
 
 function App() {
 
   return (
     <>
     {/* <Login/> */}
-    <Inicio/>
+    {/* <Inicio/> */}
     {/* <ListagemDoc/> */}
+    {/* <TelaCliente/> */}
+    <ModalFiltroFuncionario/>
     </>
   )
 }
