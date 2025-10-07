6e23430b2fe194c4887c647597a1b25af52d3cda
Fernanda Marques <fernada.anastacio@aluno.senai.br>
2025-10-07 13:37:19 -0300

    feat: Repositories, interfaces e controller de req doc e regras doc concluidos

--- BackEnd/CollabTechFile/CollabTechFile/Interfaces/IRegrasDocRepository.cs
+++ BackEnd/CollabTechFile/CollabTechFile/Interfaces/IRegrasDocRepository.cs
@@ -5,8 +5,8 @@ namespace CollabTechFile.Interfaces
     public interface IRegrasDocRepository
     {
         void Cadastrar(RegrasDoc regrasDoc);
-        void Deletar(Guid id);
-        void Editar(Guid id, RegrasDoc regrasDoc);
-
+        void Deletar(int id);
+        void Editar(int id, RegrasDoc regrasDoc);
+        List<RegrasDoc> Listar();
     }
 }
