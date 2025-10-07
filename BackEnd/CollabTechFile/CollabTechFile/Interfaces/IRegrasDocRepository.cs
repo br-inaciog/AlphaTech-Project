452e4f13a0fb48c5c5e48dc9cb5c781a1033190f
Matheus Reis <matheus.r.araujo11@aluno.senai.br>
2025-10-07 16:45:09 -0300

    feat: revisando controllers

--- BackEnd/CollabTechFile/CollabTechFile/Interfaces/IRegrasDocRepository.cs
+++ BackEnd/CollabTechFile/CollabTechFile/Interfaces/IRegrasDocRepository.cs
@@ -7,10 +7,7 @@ namespace CollabTechFile.Interfaces
         void Cadastrar(RegrasDoc regrasDoc);
         void Deletar(int id);
         void Editar(int id, RegrasDoc regrasDoc);
-<<<<<<< HEAD
         List<RegrasDoc> Listar();
-=======
 
->>>>>>> 7a9fd435835e081278a61e7344176e5a61a676d1
     }
 }
