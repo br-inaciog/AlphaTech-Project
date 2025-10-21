15d5f46f04039064b38aae83f1e5d0d60fedc1c3
Kaue Moura <Kauehago07@gmail.com>
2025-10-21 11:30:09 -0300

    feat: Tela de cadastro de cliente

--- BackEnd/CollabTechFile/CollabTechFile/obj/Debug/net8.0/CollabTechFile.AssemblyInfo.cs
+++ BackEnd/CollabTechFile/CollabTechFile/obj/Debug/net8.0/CollabTechFile.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollabTechFile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+942a08ec713c2f9fa33e85c41a138d820e19dff9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9042ac797c2ca021c128c76852469efdbbf50ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollabTechFile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollabTechFile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
