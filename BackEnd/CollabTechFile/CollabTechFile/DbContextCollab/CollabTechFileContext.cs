7f13d0db45218826025291f978c34774cc230f40
Matheus Reis <matheus.r.araujo11@aluno.senai.br>
2025-10-28 16:12:45 -0300

    Feat: Iniciando consumo de api tela de login

--- BackEnd/CollabTechFile/CollabTechFile/DbContextCollab/CollabTechFileContext.cs
+++ BackEnd/CollabTechFile/CollabTechFile/DbContextCollab/CollabTechFileContext.cs
@@ -41,11 +41,7 @@ public partial class CollabTechFileContext : DbContext
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-<<<<<<< HEAD
         => optionsBuilder.UseSqlServer("Server=NOTE36-S28\\SQLEXPRESS; Database=CollabTechFile; User Id=sa; Pwd=Senai@134; TrustServerCertificate=true;");
-=======
-        => optionsBuilder.UseSqlServer("Server = NOTE03-S28\\SQLEXPRESS; Database = CollabTechFile; User Id=sa; Pwd = Senai@134; TrustServerCertificate=true;");
->>>>>>> 386e025704063c75802f4d939b4705bb9f0161c1
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- BackEnd/CollabTechFile/CollabTechFile/obj/Debug/net8.0/CollabTechFile.AssemblyInfo.cs
+++ BackEnd/CollabTechFile/CollabTechFile/obj/Debug/net8.0/CollabTechFile.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollabTechFile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e02a7bdb99e50a1d810dff0e9dd1be7c0ab7f4ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5032d26cfc8a3198530abea78ffaee68dd55539")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollabTechFile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollabTechFile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
